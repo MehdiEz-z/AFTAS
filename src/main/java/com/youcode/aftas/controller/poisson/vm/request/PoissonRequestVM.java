f253f9d364fd7d6371aefc717f03cf163f18f1fc
Mehdi Ez-zahri <mehdiezzahriii@gmail.com>
2023-12-11 09:34:17 +0100

    Creation de Request & Response VM pour les Poissons

--- src/main/java/com/youcode/aftas/controller/poisson/vm/request/PoissonRequestVM.java
+++ src/main/java/com/youcode/aftas/controller/poisson/vm/request/PoissonRequestVM.java
@@ -1,4 +1,26 @@
 package com.youcode.aftas.controller.poisson.vm.request;
 
-public record PoissonRequestVM() {
+import com.youcode.aftas.model.entity.Niveau;
+import com.youcode.aftas.model.entity.Poisson;
+import jakarta.validation.constraints.*;
+
+public record PoissonRequestVM(
+        @NotBlank(message = "Le Nom est Obligatoire")
+        @Size(min = 4, max = 15, message = "Le nom doit avoir entre 4 et 15 caractères")
+        String nom,
+        @NotNull(message = "Le Code est Obligatoire")
+        Integer niveau,
+        @NotNull(message = "Le Poids moyen du poisson est obligatoire")
+        @Digits(integer = 10, fraction = 2, message = "Le Poids doit être une valeur numérique avec jusqu'à 2 décimales")
+        @Positive(message = "Le Poids doit être supérieur a 0")
+        Double poidsMoyen
+) {
+    public Poisson toEntite(){
+        return Poisson.builder()
+                .nomPoisson(nom)
+                .niveau(Niveau.builder()
+                        .codeNiveau(niveau)
+                        .build())
+                .build();
+    }
 }
--- src/main/java/com/youcode/aftas/controller/poisson/vm/response/PoissonResponseVM.java
+++ src/main/java/com/youcode/aftas/controller/poisson/vm/response/PoissonResponseVM.java
@@ -1,4 +1,21 @@
 package com.youcode.aftas.controller.poisson.vm.response;
 
-public class PoissonResponseVM {
+import com.youcode.aftas.model.entity.Poisson;
+
+public record PoissonResponseVM(
+        String nom,
+        Double poidsMoyen,
+        Integer code,
+        String description,
+        int points
+) {
+    public static PoissonResponseVM toVM(Poisson poisson){
+        return new PoissonResponseVM(
+            poisson.getNomPoisson(),
+            poisson.getPoidsMoyen(),
+            poisson.getNiveau().getCodeNiveau(),
+            poisson.getNiveau().getDescriptionNiveau(),
+            poisson.getNiveau().getPoints()
+        );
+    }
 }
